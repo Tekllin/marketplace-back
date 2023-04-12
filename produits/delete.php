f0ffc982a2976cba9f996747cec28850826ab297
Tekllin <111495074+Tekllin@users.noreply.github.com>
2023-04-12 10:24:45 +0200

    Add files via upload

---
+++ produits/delete.php
@@ -0,0 +1,80 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: access");
+    header("Access-Control-Allow-Methods: DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if ($method == "OPTIONS") 
+    {
+        die();
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'DELETE') :
+        http_response_code(405);
+        echo json_encode([
+            'success' => 0,
+            'message' => 'Bad Reqeust detected. HTTP method should be DELETE',
+        ]);
+        exit;
+    endif;
+
+    require '../db_connect.php';
+    $db = new Operations();
+    $cnx = $db->dbConnection();
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $idProduit =  $_GET['id'];
+
+    if (!isset($_GET['id'])) {
+        echo json_encode(['success' => 0, 'message' => 'Please provide the produit ID.']);
+        exit;
+    }
+
+    try 
+    {
+        $fetch_post = "SELECT idProduit, idAnnonceurProduit, libelleProduit, qtStock, descriptionProduit, poidsProduit, prixUnitHT, idCategorieProduit, idTvaProduit, miseEnAvant, miseEnAvantCat FROM produits WHERE idProduit = :idProduit";
+        $fetch_stmt = $cnx->prepare($fetch_post);
+        $fetch_stmt->bindValue(':idProduit', $idProduit, PDO::PARAM_INT);
+        $fetch_stmt->execute();
+
+        if ($fetch_stmt->rowCount() > 0) :
+
+            $delete_post = "DELETE FROM `produits` WHERE idProduit = :idProduit";
+            $delete_post_stmt = $cnx->prepare($delete_post);
+
+            $delete_post_stmt->bindValue(':idProduit', $idProduit, PDO::PARAM_INT);
+
+            if ($delete_post_stmt->execute()) 
+            {
+                echo json_encode([
+                    'success' => 1,
+                    'message' => 'Record Deleted successfully'
+                ]);
+                exit;
+            }
+
+            echo json_encode([
+                'success' => 0,
+                'message' => 'Could not delete. Something went wrong.'
+            ]);
+            exit;
+
+        else :
+            echo json_encode(['success' => 0, 'message' => 'Invalid ID. No produit found by these produit ID.']);
+            exit;
+        endif;
+
+    } 
+    catch (PDOException $e) 
+    {
+        http_response_code(500);
+        echo json_encode([
+            'success' => 0,
+            'message' => $e->getMessage()
+        ]);
+        exit;
+    }
+?>
---
+++ recherche/selectCategorie.php
@@ -0,0 +1,71 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: access");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Credentials: true");
+    header("Content-Type: application/json; charset=UTF-8");
+    error_reporting(E_ERROR);
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'GET') :
+        http_response_code(405);
+        echo json_encode([
+            'success' => 0,
+            'message' => 'Bad Reqeust Detected! Only get method is allowed',
+        ]);
+        exit;
+    endif;
+    
+    require '../db_connect.php';
+    $db = new Operations();
+    $cnx = $db->dbConnection();
+
+    
+    try 
+    {
+
+        $idCategorieProduit = $_GET['idCategorieProduit'];
+
+        $sql = "SELECT idProduit, idAnnonceurProduit, libelleProduit, qtStock, descriptionProduit, prixUnitHT, idCategorieProduit, libelleNomAnnonceur, 
+        AVG(CASE WHEN Avis.valid = 1 THEN Avis.noteAvis ELSE NULL END) as averageNote, MAX(Image.libelleImage) as image
+        FROM produits
+        LEFT JOIN annonceurs ON idAnnonceur = idAnnonceurProduit 
+        LEFT JOIN Avis ON Produits.idProduit = Avis.idProduitAvis
+        LEFT JOIN Image ON Produits.idProduit = Image.idProduitImage
+        WHERE idCategorieProduit = $idCategorieProduit
+        GROUP BY idProduit";
+        
+        $stmt = $cnx->prepare($sql);
+        $stmt->execute();
+        
+    
+        if ($stmt->rowCount() > 0) :
+    
+            $data = null;
+            if (is_numeric($annonceur_id)) {
+                $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            } else {
+                $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+    
+            echo json_encode([
+                'success' => 1,
+                'data' => $data,
+            ]);
+    
+        else :
+            echo json_encode([
+                'success' => 0,
+                'message' => 'No Record Found!',
+            ]);
+        endif;
+    } 
+    catch (PDOException $e) 
+    {
+        http_response_code(500);
+        echo json_encode([
+            'success' => 0,
+            'message' => $e->getMessage()
+        ]);
+        exit;
+    }
+?>
